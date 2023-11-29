6e36c50da83c29295d43504fdf7a76272129a4a5
Hasan Ali <112512209+hasanalioz@users.noreply.github.com>
2023-11-30 01:31:37 +0300

    Hadi Yükle!

---
+++ src/Core/Telegram/TelegramConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace hasanalioz\Core\Telegram;
+
+class TelegramConfig
+{
+    private static $token;
+
+    public static function setToken($token)
+    {
+        self::$token = $token;
+    }
+
+    public static function getToken()
+    {
+        return self::$token;
+    }
+}
---
+++ src/Core/Tmdb/TmdbConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace hasanalioz\Core\Tmdb;
+
+class TmdbConfig
+{
+    private static $token;
+
+    public static function setToken($token)
+    {
+        self::$token = $token;
+    }
+
+    public static function getToken()
+    {
+        return self::$token;
+    }
+}
